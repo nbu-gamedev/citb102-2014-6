bcf697f212806b54e0f4991eab52fcc2dcf8b457
Pavel Danov <pavel.danov@imperia.bg>
2014-10-15 11:39:47 +0300

    Small change to the business card program

--- f75134/business_card.cpp
+++ f75134/business_card.cpp
@@ -10,7 +10,7 @@
 // getline() - http://www.cplusplus.com/reference/string/string/getline/?kw=getline
 //
 // INFO:
-// I could've made the whole thing with "ncurses" - create borders and align the 
+// I could've made the whole thing with "ncurses" - create borders and align the
 // text with something different than "cout.width()" but I didn't have enough
 // time and I got lazy :).
 //
@@ -24,7 +24,7 @@ using namespace std;
 int main(void) {
 	string info[5];
 	string coutStringVal[5] = {"| Name:", "| City:", "| E-mail:", "| Telephone:",};
-	int nRow, nCol, nNumRows = 4, nNumCols = 4;
+	int nRow, nCol, nNumCols = 4;
 
 	int coutWidthVal[4][3] = {
 		{5, 34, 2,},
@@ -47,7 +47,7 @@ int main(void) {
 	cout.width(29); cout << right << "Your Business Card";
 	cout.width(13); cout << right << "+" << endl;
 	cout << internal << "+=========================================+" << endl;
-	
+
 	for (int col = 0; col < nNumCols; col++) {
 		nCol = 0;
 		cout.width(coutWidthVal[nRow][nCol]); cout << left << coutStringVal[col];
